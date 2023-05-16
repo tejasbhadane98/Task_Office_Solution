9d7555466cac0325cd7fa7ad9c2be74d88dc39ad
Tejas Bhadane <tejasbhadane98@gmail.com>
2023-05-16 08:02:18 +0530

    Bcrypt Added

--- routes/user.js
+++ routes/user.js
@@ -1,56 +1,116 @@
-const express = require("express");
-const User = require("../models/User");
-const router = express.Router();
-const jwt = require("jsonwebtoken");
+const express= require("express")
+const User= require("../models/User")
 
-router.post("/signUp", async(req,res)=>{
-    try{
-        const {email, password, confirmPassword}=req.body;
-        let user = await User.findOne({email});
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
+const bodyparser = require("body-parser");
+
+const router= express.Router();
+const jwt = require("jsonwebtoken")
+
+router.post("/signUp",
+    body('email').isEmail(),
+    body('password').isLength({ min: 8, max: 16 }),
+    body('confirmpassword').isLength({ min: 8, max: 16 }),async (req, res) => {
+    try {
+        // console.log(req.body)
+        let {email, password, confirmpassword} = req.body
+        let user = await User.findOne({email})
         if(user){
-            return res.json({error:"User Alredy Exists"})
-        }
-        if(password!==confirmPassword){
+            return res.json({error:"user already exits"})
+        };
+        if(password!==confirmpassword){
             return res.json({
-                error:" Password and Confirm Password Does not match!"
+                error:"Password and confirm password does not match"
             })
         }
-        user = await User.create({
-            email:email,
-            password:password
-        })
-        res.json({user, message:"Account Created Successfully"})
-    }
-    catch(error){
-        res.json({
-            message:error.message
-        })
-    }
-   
-});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // console.log("sign up error called",errors,req.body.confirmpassword)
+            return res.status(400).json({ error: "minimum length of password should be 8" });
+        }
+        
+      
+        
+       
+        
+        bcrypt.hash(password, 10, async function(err, hash){
+            // It Will Store hash in the password DB.
+            
+            if (err) {
+                return res.status(500).json({
+                    status: "Failed",
+                    message: err.message
+                })
+            }
 
-router.post("/signIn", async(req, res)=>{
-    try{
-        let {email, password} = req.body
-        let user = await User.findOne({email});
-        if(!user){
-            return res.json({
-                error: "User Does Not Exists"
+            user= await User.create({
+                
+                email: email,                        // It will return the user value in these format
+                password: hash
             })
-        }
-        if(password!==user.password){
-            return res.json({
-                error:"Incorrect Password"
-            })
-        }
-        let token = jwt.sign({_id:user._id}, process.env.JWT_SECRET)
-        res.json({user,token, message:"Logged In Successfully"})
-    }
-    catch(error){
+            res.json({user, message:"Account created"})
+
+
+        })
+
+       
+    } catch (error) {
         res.json({
-            message:error.message
+            messgae:error.message
         })
     }
+
 })
 
-module.exports = router;
+router.post("/signIn",
+body('email').isEmail(),
+async (req,res)=>{
+
+    try {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: "email is not valid" });
+        }
+
+
+        let {email, password} = req.body
+        let user = await User.findOne({email})
+        if(!user){
+           return res.json({
+                error:"user does not exists"
+            })
+        } 
+
+  
+
+        bcrypt.compare(password, user.password, function(err, result){
+            if(err){
+                return res.status(500).json({
+                    status:"Failed",
+                    message:err.message
+                })
+            }
+            if(result){
+                const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+                res.json({user,token, message:"logged in succesfully"})
+            }
+            else if(password!=user.password){
+                return res.status(405).json({
+                    error:"Incorrect Password"
+                })
+            }
+        })
+        
+
+    } catch (error) {
+        res.json({
+            messgae:error.message
+        })
+    }
+
+})
+
+
+module.exports = router
